89ae65f2ed8a660284655f3b925e08bc00072ef2
cailo2042 <105643442+cailo2042@users.noreply.github.com>
2022-05-24 21:35:27 +0300

    Add files via upload

---
+++ listmani.py
@@ -0,0 +1,15 @@
+# for loops with lists.
+words=["I","DID","A"]
+for word in words:
+    print(word+"i")
+
+mangoes="book"
+for i in mangoes:
+    print("Done")
+print(i)
+# counting letters in a string
+str="Hello welcome back to my class"
+count=0.
+for x in str:
+    if(x=='o'):
+        count +=1

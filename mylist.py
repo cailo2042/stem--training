1725863103174ea258b276564530a19bde44bb12
cailo2042 <105643442+cailo2042@users.noreply.github.com>
2022-05-24 21:37:02 +0300

    Add files via upload

---
+++ mylist.py
@@ -0,0 +1,29 @@
+my_list=[2,3,4,5,6,34]
+for i in my_list:
+        print(i)
+other_list=[]    
+my_list=[2,3,4,5,6,34]    
+for i in my_list:
+    other_list.append(i)
+print(other_list)
+my_list=[2,4,1,7,8,10,14]
+other_list=["i for i in my list"]
+print (other_list)
+for elem in my_list:
+    if elem%2==0:
+        other_list.append(elem)
+print(other_list)        
+my_list=[2,4,1,7,8,10,12]
+other_list=["i for elem in my list"]
+print(other_list)
+my_list=[2,4,1,7,8,10,12]
+other_list=[i for i in my_list if i %2==0]
+print(i)
+def add(a,b):
+    return a+b
+result=add (4,6) 
+print(result)
+result_2=add(5,8)
+print(result_2)
+
+
